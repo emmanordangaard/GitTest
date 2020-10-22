e72f86be385becc18bff68ec975e0ef4f685d639
emmanordangaard@gmail.com <emmanordangaard@gmail.com>
2020-10-22 15:14:52 +0200

    Sqqr to cube

--- GitTest.1/Program.cs
+++ GitTest.1/Program.cs
@@ -13,7 +13,7 @@ namespace GitTest._1
             // Console.WriteLine("Hello World!");
             for (int i = 1; i < 10; i++)
             {
-                Console.WriteLine("{0}^2 = {1}", i, i * i);
+                Console.WriteLine("{0}^3 = {1}", i, i * i * i);
             }
             Console.ReadKey();
         }
